67f3690e7a548ef3ad06db628a66af03f4301b5e
NathanZorndorf <nzorndorf@gmail.com>
2020-09-23 16:15:33 -0700

    Created first tutorial.

---
+++ _archive/2020-09-22-first-post.md
@@ -0,0 +1,21 @@
+---
+title: "First Post... EVER!!"
+excerpt_separator: "<!--more-->"
+categories:
+  - Blog
+tags:
+  - Firsts
+---
+
+Hi all! This is my first post ever on my brand new, personal website. 
+
+Here's some code:
+```python
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('file.txt')
+
+df.head()
+```
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Example: Performing a Chi-Square Goodness-of-Fit Test in Python",
+        "excerpt":"Chapter 10 Examples &amp; Problems Test for goodness of fit of the normal-probability model&#182; Taken from page 426 of Funamentals of Biostatistics, 8th Edition, by Bernard Rosner. For this example, we will utilize the test data from the following hypothetical experiment where diastolic blood-pressure measurements were collected at home in...","categories": ["Blog"],
+        "tags": ["Chi-square","Python","Example"],
+        "url": "http://localhost:4000/blog/goodness-of-fit/",
+        "teaser": null
+      }]
